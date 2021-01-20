54a9399a221662c4dbf1630a22ea4230434b81f6
Nikita Ivanov <mybestjob@myself.com>
2021-01-20 21:42:24 +0300

    Initial changes applied

--- js/app.js
+++ js/app.js
@@ -25,6 +25,53 @@
  * 
 */
 
+function isElementInViewport (el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+var oldVisible = new Object();
+let headers = document.querySelector('main section div h2');
+for(let i = 0; i < headers.length; i++){
+    oldVisible['h'+i] = false;
+}
+
+function onVisibilityEnabled(header, i, callbackIfTrue){    
+    var visible = isElementInViewport(header);
+    if(visible != oldVisible[i]){
+        oldVisible[i] = visible;
+    }
+    if(visible === true && typeof callbackIfTrue == 'function'){
+        callbackIfTrue();
+    }
+
+}
+
+function deselectAllSections(sections){
+    for (section of sections){
+        section.classList.remove('your-active-class');
+    }
+}
+
+function selectVisibleSection(section){
+    section.classList.add('your-active-class');
+}
+
+function deselectAllNavbar(navbar){
+    for (n of navbar){
+        n.classList.remove('your-active-class');
+    }
+}
+
+function selectVisibleNavbarElement(navElement){
+    navElement.classList.add('your-active-class');
+}
+
 
 
 /**
@@ -34,10 +81,21 @@
 */
 
 // build the nav
-
+const fragment = document.createDocumentFragment();
+let sections = document.querySelectorAll('main section');
+for(var s of sections){
+    var link = document.createElement('a');
+    link.textContent = s.dataset.nav;
+    link.href = '#'+s.id;
+    var menuElement = document.createElement('li');
+    menuElement.className = 'menu__link';
+    menuElement.id = 'Nav'+s.id;
+    menuElement.appendChild(link);
+    fragment.appendChild(menuElement);
+}
+document.querySelector('#navbar__list').appendChild(fragment);
 
 // Add class 'active' to section when near top of viewport
-
 
 // Scroll to anchor ID using scrollTO event
 
@@ -53,5 +111,24 @@
 // Scroll to section on link click
 
 // Set sections as active
+var handler = function(){
+    let headers = document.querySelectorAll('.landing__container h2');
+    let sections = document.querySelectorAll('body main section');
+    let navbar = document.querySelectorAll('.navbar__menu .menu__link');
+    
+    for(let i = 0; i < headers.length; i++){
+        let h = headers[i];
+        onVisibilityEnabled(h, 'h'+i,function(){
+            deselectAllSections(sections);
+            selectVisibleSection(sections[i]);
+            deselectAllNavbar(navbar);
+            selectVisibleNavbarElement(navbar[i]);
+        });
+    }
+}
 
+window.addEventListener('DOMContentLoaded', handler);
+window.addEventListener('load', handler);
+window.addEventListener('scroll', handler);
+window.addEventListener('resize', handler);
 
